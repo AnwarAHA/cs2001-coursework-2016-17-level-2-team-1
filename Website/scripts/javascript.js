88f0520d8a694193a599be01868c8a9e0bf1d4e9
George Brougham <gpabrougham@gmail.com>
2017-03-22 16:36:15 +0000

    Website and video
    Includes HTML website, CSS, JavaScript and jQuery scripts as well as media including the promotional video.

---
+++ Website/scripts/javascript.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "50vw";
+    document.getElementById("content").style.marginLeft = "50vw";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("content").style.margin= "0 auto";
+}
